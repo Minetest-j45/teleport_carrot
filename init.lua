18a7ed5372f0c5957c488de089015ceeec8d901a
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-11-24 09:56:53 +0000

    update

--- init.lua
+++ init.lua
@@ -1,29 +1,28 @@
-minetest.register_craftitem("teleportcarrot:carrot", {
+minetest.register_craftitem("teleport_carrot:carrot", {
 	description = "Teleporting carrot",
 	inventory_image = "telecarrot.png",
 	wield_image = "telecarrot.png",
-	on_use = function(itemstack, user, pointed_thing)
-	   minetest.item_eat(2)
-	   math.randomseed(os.time())
+	on_use = function(itemstack, user)
+	   local hp = user:get_hp()
+	   user:set_hp(hp + 2)
 	   local random1 = math.random(1, 10)
 	   if random1 == 1 then
-	      local randomx = math.random(-16, 16)
-	      local randomz = math.random(-16, 16)
-	      local xpos = user:get_pos().x + randomx
-          local ypos = user:get_pos().y
-          local zpos = user:get_pos().z + randomz
-	      user:set_pos({x = xpos, y =ypos, z =zpos})
-	   itemstack:take_item(1)
-	   return item
-	end
-end,
+	    	local randomx = math.random(-16, 16)
+	    	local randomz = math.random(-16, 16)
+	    	local xpos = user:get_pos().x + randomx
+        	local zpos = user:get_pos().z + randomz
+	    	user:set_pos({x = xpos, y =user:get_pos().y, z =zpos})
+		end
+		itemstack:take_item(1)
+	  	return itemstack
+	end,
 })
 
 minetest.register_craft({
-	output = "teleportcarrot:carrot",
+	output = "teleport_carrot:carrot",
 	recipe = {
-		{"default:obsidian", "default:obsidian", "default:obsidian"},
+		{"", "default:obsidian", ""},
 		{"default:obsidian", "farming:carrot_gold", "default:obsidian"},
-		{"default:obsidian", "default:obsidian", "default:obsidian"},
+		{"", "default:obsidian", ""},
 	}
 })
